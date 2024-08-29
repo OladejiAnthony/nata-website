b5549414b9becac02c4130f9dd4d2a2002cf62e3
oladeji anthony <oladejianthony98@gmail.com>
2024-08-29 22:47:42 +0100

    contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,101 @@
-import React from 'react'
+import React from "react";
+import "../styles/ContactPage.scss";
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div className="contact__page">
+      <div className="heading">
+        <div className="header__box">
+          <div className="upper_line"></div>
+          <h2>NATA</h2>
+          <div className="lower_line"></div>
+        </div>
+      </div>
+      <div className="textContainer">
+        <p>
+          The Nigeria Automobile Technicians Association, NATA, is a national
+          professional body of the micro, small and medium scale auto-repairers
+          in the informal economy. Members include artisans without formal
+          schooling and technicians with average formal education and belong to
+          such sub-trade groups like vulcanizing, auto-electrical repairing,
+          panel fixing, auto-body building, motor engine repairing and
+          servicing, welding and iron bending, auto-spraying and other auto
+          related works.The Nigerian Automobile Technicians association, NATA,
+          with over two million members from all over the federation has an
+          history that dates back to 1919. It was officially registered as a
+          registered industrial union in June 2nd 1962 as Nigeria Motor Mechanic
+          Association, it was re-registered in 1986 as Nigeria Motor Mechanics
+          and Technicians Association when the federal government excluded all
+          trade unions of self employed workers from the registered trade
+          unions, it was re-registered in 1986 by the then minister for internal
+          affairs, Colonel John Shagaya whose ministry was solely empowered to
+          register association before the establishment of Corporate Affairs
+          Commission. In june 2000, the association was re-registered at the
+          Corporate Affairs Commission with a new name- Nigeria Automobile
+          Technicians Association, with certificate number RC 4596. Members
+          constitute over 75% of all automobile technicians in Nigeria, NATA has
+          more men membership than women, about 5% of the population are women
+          and they are in few states like Lagos, Anambra, oyo, Rivers, Abuja and
+          Edo state
+        </p>
+      </div>
 
-export default ContactPage
+      <div className="heading">
+        <div className="header__box">
+          <div className="upper_line"></div>
+          <h2>NATA Lagos</h2>
+          <div className="lower_line"></div>
+        </div>
+      </div>
+      <div className="textContainer">
+        <p>
+          Welcome to the National Association of Auto Technicians, NATA, Lagos
+          State Chapter. We are a dedicated and professional organization
+          committed to advancing the skills, knowledge, and professionalism of
+          auto technicians across Lagos State. Our association serves as a
+          central hub for automotive professionals, providing resources,
+          training, and support to enhance their expertise and elevate the
+          standards of the auto repair industry.
+        </p>
+      </div>
 
+      <div className="heading">
+        <div className="header__box">
+          <div className="upper_line"></div>
+          <h2>History</h2>
+          <div className="lower_line"></div>
+        </div>
+      </div>
+      <div className="textContainer">
+        <p>
+          The National Association of Auto Technicians, NATA, in Lagos State has
+          a rich history rooted in the passion and dedication of pioneering auto
+          technicians. Established in [Year of Establishment], NATA was founded
+          with the goal of uniting auto professionals, standardizing practices,
+          and advocating for the advancement of the automotive repair industry.
+          In its early years, NATA focused on creating a cohesive community of
+          auto technicians, providing a platform for networking, knowledge
+          sharing, and professional development. As the automotive industry
+          evolved, so did NATA, expanding its programs and services to include
+          comprehensive training, certification, and quality assurance
+          initiatives. Over the years, NATA has grown to become a respected and
+          influential organization within Lagos State and beyond. The
+          association has played a pivotal role in enhancing the skills of auto
+          technicians, promoting ethical practices, and improving service
+          quality in the industry. Through partnerships with educational
+          institutions, industry stakeholders, and government bodies, NATA has
+          been instrumental in shaping policies and standards that benefit both
+          technicians and consumers. Today, NATA continues to uphold its
+          founding principles, striving to elevate the status of auto
+          technicians and ensure that they are well-equipped to meet the
+          challenges of a rapidly changing automotive landscape. With a focus on
+          continuous learning, innovation, and community engagement, NATA
+          remains committed to its mission of excellence in the automotive
+          repair industry.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import "../styles/ServicesPage.scss";
+import service1 from "../assets/images/automobile.png";
+import service2 from "../assets/images/welder.png";
+import service3 from "../assets/images/panel-beater.png";
+import service4 from "../assets/images/auto-sprayer.png";
+import service5 from "../assets/images/electricians.png";
+import service6 from "../assets/images/brake.png";
+import service7 from "../assets/images/ac.png";
+import service8 from "../assets/images/blacksmith.png";
+import service9 from "../assets/images/upholstery.png";
 
 const ServicesPage = () => {
   return (
-    <div>ServicesPage</div>
-  )
-}
+    <div className="services__Page">
+      <div className="heading">
+        <div className="header__box">
+          <div className="upper_line"></div>
+          <h2>List of Services</h2>
+          <div className="lower_line"></div>
+        </div>
+      </div>
+      <div className="services__images">
+        <div className="service_image">
+          <img src={service1} alt="service1" />
+          <h3>AUTOMOBILE MECHANICS</h3>
+        </div>
+        <div className="service_image">
+          <img src={service2} alt="service2" />
+          <h3>WELDER</h3>
+        </div>
+        <div className="service_image">
+          <img src={service3} alt="service3" />
+          <h3>PANEL BEATING</h3>
+        </div>
+        <div className="service_image">
+          <img src={service4} alt="service4" />
+          <h3>AUTO SPRAYERS</h3>
+        </div>
+        <div className="service_image">
+          <img src={service5} alt="service5" />
+          <h3>AUTO ELECTRICIANS</h3>
+        </div>
+        <div className="service_image">
+          <img src={service6} alt="service6" />
+          <h3>AUTO BRAKE SPECIALIST</h3>
+        </div>
+        <div className="service_image">
+          <img src={service9} alt="service9" />
+          <h3>AUTO UPHOLSTERY SPECIALIST</h3>
+        </div>
+        <div className="service_image">
+          <img src={service7} alt="service7" />
+          <h3>AUTO A/C SPECIALIST</h3>
+        </div>
+        <div className="service_image">
+          <img src={service8} alt="service8" />
+          <h3>AUTO BLACKSMITH</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ServicesPage
-
+export default ServicesPage;
 
