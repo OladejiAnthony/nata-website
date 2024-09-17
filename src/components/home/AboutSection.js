874b7dcbb0bbd298494633b571364dbf4080c56e
oladeji anthony <oladejianthony98@gmail.com>
2024-09-17 08:14:01 +0100

    navbar responsiveness

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import "./AboutSection.scss"
 import abt from "../../assets/images/about-left.png"
+import { Link } from 'react-router-dom'
 
 const AboutSection = () => {
   return (
@@ -18,7 +19,7 @@ const AboutSection = () => {
         <p>
           The Nigerian Automobile Technicians Association (NATA) with over two million members from all over the federation has a history that dates back to 1919. It was officially registered as a registered industrial union on June 2nd, 1962 as the Nigeria Motor Mechanic Association. It was re-registered in 1986 as the Nigeria Motor Mechanics and Technicians Association when the federal government excluded all trade unions of self-employed workers from the registered trade unions. It was re-registered in 1986 by ...
         </p>
-        <button>Learn more</button>
+        <Link to="/about">Learn more</Link>
       </div>
     </div>
   )
